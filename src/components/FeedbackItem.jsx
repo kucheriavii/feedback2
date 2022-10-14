853503a12b0526bbee7a7ee93149125cd31b8788
kucheriavii <kucheriavii@gmail.com>
2022-10-14 22:07:21 +0300

    4. Managing Global State

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,21 +1,11 @@
 import React, { useState } from 'react'
 
-export default function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setext] = useState('This is an example of a feedback item');
-
-  const handleClick = () => {
-    setRating((prev) => {
-        console.log(prev);
-        return prev+1;
-    })
-  }
-
-
+export default function FeedbackItem({item}) {
+ 
     return (
     <div className='card'>
-        <div className='num-display'>{rating}</div>
-        <div className='text-display'>{text}</div>
+        <div className='num-display'>{item.rating}</div>
+        <div className='text-display'>{item.text}</div>
     </div>
   )
 }
